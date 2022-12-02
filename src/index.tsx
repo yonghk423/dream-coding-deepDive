d977fda450db7896fd63daecbc01607d59c76931
yonghk423 <yonghk423423@gmail.com>
2022-12-02 22:42:56 +0900

    error & loading

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import AppMentor from "./AppMentor";
 import AppMentors from "./AppMentors";
+import AppProducts from "./basic/AppProducts";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <AppMentor /> */}
-    <AppMentors />
+    {/* <AppMentors /> */}
+    <AppProducts />
   </React.StrictMode>
 );
